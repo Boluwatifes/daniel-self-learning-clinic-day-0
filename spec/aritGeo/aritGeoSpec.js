3f5f400be8051670e3fec15ae0d6a82a79471e13
Bamidele Daniel <greatbolutife@gmail.com>
2017-04-08 10:25:23 +0100

    aritGeo test function added

---
+++ spec/aritGeo/aritGeoSpec.js
@@ -0,0 +1,80 @@
+(function(){
+  'use strict';
+
+  var aritGeoLib = require('../../lib/aritGeo/aritGeo.js');
+  let ag = aritGeoLib;
+
+  describe("Determine the sequence of an array of numbers: ", function() {
+
+    describe("Case for an empty array", function() {
+
+      it("should return 0 for an empty array", function() {
+        expect(ag.aritGeo([])).toEqual(0);
+      });
+
+    });
+
+    describe("Case for an arithmetic sequence", function() {
+
+      it("should return `Arithmetic` for [2, 4, 6, 8, 10]", function() {
+        expect(ag.aritGeo([2, 4, 6, 8, 10])).toEqual('Arithmetic');
+      });
+
+      it("should return `Arithmetic` for [5, 11, 17, 23, 29, 35, 41]", function() {
+        expect(ag.aritGeo([5, 11, 17, 23, 29, 35, 41])).toEqual('Arithmetic');
+      });
+
+      it("should return `Arithmetic` for [15, 10, 5, 0, -5, -10]", function() {
+        expect(ag.aritGeo([15, 10, 5, 0, -5, -10])).toEqual('Arithmetic');
+      });
+
+    });
+
+    describe("Case for a geometric sequence", function() {
+
+      it("should return `Geometric` for [2, 6, 18, 54, 162]", function() {
+        expect(ag.aritGeo([2, 6, 18, 54, 162])).toEqual('Geometric');
+      });
+
+      it("should return `Geometric` for [0.5, 3.5, 24.5, 171.5]", function() {
+        expect(ag.aritGeo([0.5, 3.5, 24.5, 171.5])).toEqual('Geometric');
+      });
+
+      it("should return `Geometric` for [−128, 64, −32, 16, −8]", function() {
+        expect(ag.aritGeo([-128, 64, -32, 16, -8])).toEqual('Geometric');
+      });
+
+    });
+
+    describe("Case for neither arithmetic nor geometric sequence", function() {
+
+      it("should return -1 for [1, 2, 3, 5, 8]", function() {
+        expect(ag.aritGeo([1, 2, 3, 5, 8])).toEqual(-1);
+      });
+
+      it("should return -1 for [1, 3, 6, 10, 15]", function() {
+        expect(ag.aritGeo([1, 3, 6, 10, 15])).toEqual(-1);
+      });
+
+      it("should return -1 for [1, 8, 27, 64, 125]", function() {
+        expect(ag.aritGeo([1, 8, 27, 64, 125])).toEqual(-1);
+      });
+      
+    });
+
+    describe("Case for invalid inputs", function(){
+
+      it("Should return `Invalid inputs` for `andela`", function(){
+        expect(ag.aritGeo('andela')).toEqual('Invalid inputs');
+      });
+
+      it("Should return `Invalid inputs` for `bootcamp2017`", function(){
+        expect(ag.aritGeo('bootcamp2017')).toEqual('Invalid inputs');
+      });
+
+      it("Should return `Invalid inputs` for `[3, 'a', 6]`", function(){
+        expect(ag.aritGeo([3, 'a', 6])).toEqual('Invalid inputs');
+      });
+    })
+  });
+})();
